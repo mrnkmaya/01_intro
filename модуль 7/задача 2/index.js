590bbdf062e5ba4f183b30de72be86a14445bf90
mironyuk maya <mayamir300105@gmail.com>
2024-12-09 10:45:57 +0500

    add task2

--- модуль 7/задача 2/index.js
+++ модуль 7/задача 2/index.js
@@ -2,22 +2,44 @@ let heightBtn = document.querySelector('.heightBtn');
 let filterBtn = document.querySelector('.filterBtn');
 let heightList = document.querySelector('.heightList');
 
+function filter(arr, min) {
+    const result = []
+    for (const item of arr) {
+        if (item >= min) {
+            result.push(item)
+        }
+    }
+    return result
+}
+
 let heights = [164, 157, 160, 143, 170]
 
-for (const element of heights) {
-    let liEl = document.createElement('li');
-    liEl.textContent = element
-    heightList.append(liEl)
+function renderList(heights){
+    heightList.innerHTML = ''
+    for (const element of heights) {
+        let liEl = document.createElement('li');
+        liEl.textContent = element
+        heightList.append(liEl)
+    }
 }
+
 
 
 heightBtn.onclick = function () {
     let newHeight = prompt('Введите рост')
+    
     if (!newHeight) {
         alert('Рост не введён!')
+    } else {
+        heights.push(newHeight)
+        renderList(heights)
     }
 }
 
 filterBtn.onclick = function () {
-    alert('fkdkfkdk')
+    let min = prompt('Введите минимальное значение')
+    const result = filter(heights, min)
+    renderList(result)
 }
+
+renderList(heights)
